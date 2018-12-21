30366826c6b28898026714f80db43f5bfd5fd92c
RubaXa <ibnRubaXa@gmail.com>
2018-12-21 13:13:38 +0300

    * v2.0.26

--- dist/FileAPI.html5.js
+++ dist/FileAPI.html5.js
@@ -1,4 +1,4 @@
-/*! FileAPI 2.0.25 - BSD | git://github.com/mailru/FileAPI.git
+/*! FileAPI 2.0.26 - BSD | git://github.com/mailru/FileAPI.git
  * FileAPI — a set of  javascript tools for working with files. Multiupload, drag'n'drop and chunked file upload. Images: crop, resize and auto orientation by EXIF.
  */
 
@@ -287,7 +287,7 @@
 		 * FileAPI (core object)
 		 */
 		api = {
-			version: '2.0.25',
+			version: '2.0.26',
 
 			cors: false,
 			html5: true,
